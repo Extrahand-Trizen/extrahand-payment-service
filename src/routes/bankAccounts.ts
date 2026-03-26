390d1c5613dea44494159df6e540ee128097e02d
Asish <asishvenkat.a2004@gmail.com>
2026-03-26 09:54:46 +0530

    updates

--- src/routes/bankAccounts.ts
+++ src/routes/bankAccounts.ts
@@ -5,7 +5,10 @@ import { BankAccountController } from '../controllers/BankAccountController';
 
 const router = express.Router();
 
-router.use(serviceAuthMiddleware);
+const disableServiceAuth = process.env.DISABLE_SERVICE_AUTH_FOR_BANK_ACCOUNTS === 'true';
+if (!disableServiceAuth) {
+	router.use(serviceAuthMiddleware);
+}
 
 router.post('/', asyncHandler(BankAccountController.upsertBankAccount));
 router.get('/me', asyncHandler(BankAccountController.getMyBankAccounts));

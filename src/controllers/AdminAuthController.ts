58eb4b6faa1b18a058170c8e3e8ab54f27635949
Tarun516 <taruncv516@gmail.com>
2026-04-01 14:00:56 +0530

    changes

--- src/controllers/AdminAuthController.ts
+++ src/controllers/AdminAuthController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { asyncHandler } from '../middleware/errorHandler';
-import { getAdminUser, verifyPassword } from '../services/adminAuthService';
+import { getAdminUser, verifyPassword } from '../services/adminAuthService'; 
  
 export class AdminAuthController {
   static login = asyncHandler(async (req: Request, res: Response) => {
